435b03216503fe2b1118b750cb3b4a45796620ad
Andrew D Smith <andrewds@usc.edu>
2022-09-28 16:51:02 -0700

    Adding the copyright info

--- R/model_comparisons_levels.R
+++ R/model_comparisons_levels.R
@@ -1,3 +1,21 @@
+## model_comparisons_levels.R: compare mixed model fits for
+## methylation levels
+##
+## Copyright (C) 2022 Andrew D Smith and Guilherme de Sena Brandine
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 library(lmerTest) # Mixed model package
 library(RLRsim) # For testing mixed models by simulation
 
