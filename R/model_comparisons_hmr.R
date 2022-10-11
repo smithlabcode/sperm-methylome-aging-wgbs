2408f9ea72a3cff1815712e54c7ffba54a5afe47
Andrew D Smith <andrewds@usc.edu>
2022-10-11 15:30:00 -0700

    Changing model_comparisons_hmr.R model_comparisons_levels.R into functions

--- R/model_comparisons_hmr.R
+++ R/model_comparisons_hmr.R
@@ -16,43 +16,48 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-library(lmerTest) # Mixed model package
-library(RLRsim) # For testing mixed models by simulation
+model_comparisons_hmr <- function(input_filename, # file with data for samples
+                                  ages_filename, # file with ages for samples
+                                  # do 1M simulations for exactRLRT tests
+                                  n_simulations = 1000000) {
 
-X <- read.table(input_filename, header=T)
-ages_table <- read.table(ages_filename, header=T)
+  library(lmerTest) # Mixed model package
+  library(RLRsim) # For testing mixed models by simulation
 
-stopifnot("subjects must be identical between input files" =
+  X <- read.table(input_filename, header=T)
+  ages_table <- read.table(ages_filename, header=T)
+
+  stopifnot("subjects must be identical between input files" =
               X$subject == ages_table$subject)
 
-stopifnot("column headings must be subject coverage count size" =
+  stopifnot("column headings must be subject coverage count size" =
               c('subject', 'coverage', 'count', 'size') == colnames(X))
 
-X$age <- ages_table$age
+  X$age <- ages_table$age
 
-use_reml <- TRUE # use restricted max likelihood
-n_simulations <- 10000000 # do 1M simulations for exactRLRT tests
+  use_reml <- TRUE # use restricted max likelihood
 
-### First test
-### H0: HMR coverage is independent of age
+  ### First test
+  ### H0: HMR coverage is independent of age
 
-X$coverage <- scale(X$coverage)
-coverage_model <- lmer(coverage ~ age + (1|subject), data = X, REML=use_reml)
-print(summary(coverage_model))
-print(exactRLRT(coverage_model, nsim=n_simulations))
+  X$coverage <- scale(X$coverage)
+  coverage_model <- lmer(coverage ~ age + (1|subject), data = X, REML=use_reml)
+  print(summary(coverage_model))
+  print(exactRLRT(coverage_model, nsim=n_simulations))
 
-### Second test
-### H0: number of HMRs is independent of age
+  ### Second test
+  ### H0: number of HMRs is independent of age
 
-X$count <- scale(X$count)
-count_model <- lmer(count ~ (1|subject) + age, data = X, REML=use_reml)
-print(summary(count_model))
-print(exactRLRT(count_model, nsim=n_simulations))
+  X$count <- scale(X$count)
+  count_model <- lmer(count ~ (1|subject) + age, data = X, REML=use_reml)
+  print(summary(count_model))
+  print(exactRLRT(count_model, nsim=n_simulations))
 
-### Third test
-### H0: HMR size is independent of age
+  ### Third test
+  ### H0: HMR size is independent of age
 
-X$size <- scale(X$size)
-size_model <- lmer(size ~ (1|subject) + age, data = X, REML=use_reml)
-print(summary(size_model))
-print(exactRLRT(size_model, nsim=n_simulations))
+  X$size <- scale(X$size)
+  size_model <- lmer(size ~ (1|subject) + age, data = X, REML=use_reml)
+  print(summary(size_model))
+  print(exactRLRT(size_model, nsim=n_simulations))
+}
--- R/model_comparisons_levels.R
+++ R/model_comparisons_levels.R
@@ -16,43 +16,48 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-library(lmerTest) # Mixed model package
-library(RLRsim) # For testing mixed models by simulation
+model_comparisons_levels <- function(input_filename, # file with data for samples
+                                     ages_filename, # file with ages for samples
+                                     # do 1M simulations for exactRLRT tests
+                                     n_simulations = 1000000) {
 
-X <- read.table(input_filename, header=T)
-ages_table <- read.table(ages_filename, header=T)
+  library(lmerTest) # Mixed model package
+  library(RLRsim) # For testing mixed models by simulation
 
-stopifnot("subjects must be identical between input files" =
+  X <- read.table(input_filename, header=TRUE)
+  ages_table <- read.table(ages_filename, header=TRUE)
+
+  stopifnot("subjects must be identical between input files" =
               X$subject == ages_table$subject)
 
-stopifnot("column headings must be subject mean weighted fractional" =
+  stopifnot("column headings must be subject mean weighted fractional" =
               c('subject', 'mean', 'weighted', 'fractional') == colnames(X))
 
-X$age <- ages_table$age
+  X$age <- ages_table$age
 
-use_reml <- TRUE # use restricted max likelihood
-n_simulations <- 10000000 # do 1M simulations for exactRLRT tests
+  use_reml <- TRUE # use restricted max likelihood
 
-### First test
-### H0: mean methylation is independent of age
+  ## First test
+  ## H0: mean methylation is independent of age
 
-X$mean <- scale(X$mean)
-mean_model <- lmer(mean ~ age + (1|subject), data = X, REML=use_reml)
-print(summary(mean_model))
-print(exactRLRT(mean_model, nsim=n_simulations))
+  X$mean <- scale(X$mean)
+  mean_model <- lmer(mean ~ age + (1|subject), data = X, REML=use_reml)
+  print(summary(mean_model))
+  print(exactRLRT(mean_model, nsim=n_simulations))
 
-### Second test
-### H0: weighted mean methylation is independent of age
+  ## Second test
+  ## H0: weighted mean methylation is independent of age
 
-X$weighted <- scale(X$weighted)
-weighted_model <- lmer(weighted ~ (1|subject) + age, data = X, REML=use_reml)
-print(summary(weighted_model))
-print(exactRLRT(weighted_model, nsim=n_simulations))
+  X$weighted <- scale(X$weighted)
+  weighted_model <- lmer(weighted ~ (1|subject) + age, data = X, REML=use_reml)
+  print(summary(weighted_model))
+  print(exactRLRT(weighted_model, nsim=n_simulations))
 
-### Third test
-### H0: fraction methylated is independent of age
+  ## Third test
+  ## H0: fraction methylated is independent of age
 
-X$fractional <- scale(X$fractional)
-fractional_model <- lmer(fractional ~ (1|subject) + age, data = X, REML=use_reml)
-print(summary(fractional_model))
-print(exactRLRT(fractional_model, nsim=n_simulations))
+  X$fractional <- scale(X$fractional)
+  fractional_model <- lmer(fractional ~ (1|subject) + age, data = X, REML=use_reml)
+  print(summary(fractional_model))
+  print(exactRLRT(fractional_model, nsim=n_simulations))
+}
