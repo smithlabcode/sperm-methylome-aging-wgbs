2e6c58bb09971c0adc0ce4b184b0f19b750d1d91
Andrew D Smith <andrewds@usc.edu>
2022-10-18 11:27:42 -0700

    Updating the model_comparison_hmr.R so it includes age and sample in one data file

--- R/model_comparisons_hmr.R
+++ R/model_comparisons_hmr.R
@@ -17,7 +17,6 @@
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 model_comparisons_hmr <- function(input_filename, # file with data for samples
-                                  ages_filename, # file with ages for samples
                                   # do 1M simulations for exactRLRT tests
                                   n_simulations = 1000000) {
 
@@ -25,23 +24,16 @@ model_comparisons_hmr <- function(input_filename, # file with data for samples
   library(RLRsim) # For testing mixed models by simulation
 
   X <- read.table(input_filename, header=T)
-  ages_table <- read.table(ages_filename, header=T)
-
-  stopifnot("subjects must be identical between input files" =
-              X$subject == ages_table$subject)
-
-  stopifnot("column headings must be subject coverage count size" =
-              c('subject', 'coverage', 'count', 'size') == colnames(X))
-
-  X$age <- ages_table$age
-
+  stopifnot("column headings must sample donor age coverage count size" =
+              c('sample', 'donor', 'age',
+                'coverage', 'count', 'size') == colnames(X))
   use_reml <- TRUE # use restricted max likelihood
 
   ### First test
   ### H0: HMR coverage is independent of age
 
   X$coverage <- scale(X$coverage)
-  coverage_model <- lmer(coverage ~ age + (1|subject), data = X, REML=use_reml)
+  coverage_model <- lmer(coverage ~ age + (1|donor), data = X, REML=use_reml)
   print(summary(coverage_model))
   print(exactRLRT(coverage_model, nsim=n_simulations))
 
@@ -49,7 +41,7 @@ model_comparisons_hmr <- function(input_filename, # file with data for samples
   ### H0: number of HMRs is independent of age
 
   X$count <- scale(X$count)
-  count_model <- lmer(count ~ (1|subject) + age, data = X, REML=use_reml)
+  count_model <- lmer(count ~ (1|donor) + age, data = X, REML=use_reml)
   print(summary(count_model))
   print(exactRLRT(count_model, nsim=n_simulations))
 
@@ -57,7 +49,7 @@ model_comparisons_hmr <- function(input_filename, # file with data for samples
   ### H0: HMR size is independent of age
 
   X$size <- scale(X$size)
-  size_model <- lmer(size ~ (1|subject) + age, data = X, REML=use_reml)
+  size_model <- lmer(size ~ (1|donor) + age, data = X, REML=use_reml)
   print(summary(size_model))
   print(exactRLRT(size_model, nsim=n_simulations))
 }
